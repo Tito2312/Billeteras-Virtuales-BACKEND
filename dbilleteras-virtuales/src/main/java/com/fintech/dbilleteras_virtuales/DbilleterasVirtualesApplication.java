4d07afb2e8d1850a11f72ecf94c32e0dcf1e7682
cesarandres17 <cesara.ramirezr@uqvirtual.edu.co>
2026-04-11 10:07:25 -0500

    Estructura SpringBoot

---
+++ dbilleteras-virtuales/src/main/java/com/fintech/dbilleteras_virtuales/DbilleterasVirtualesApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.dbilleteras_virtuales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbilleterasVirtualesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbilleterasVirtualesApplication.class, args);
+	}
+
+}
---
+++ dbilleteras-virtuales/src/test/java/com/fintech/dbilleteras_virtuales/DbilleterasVirtualesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.dbilleteras_virtuales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbilleterasVirtualesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
