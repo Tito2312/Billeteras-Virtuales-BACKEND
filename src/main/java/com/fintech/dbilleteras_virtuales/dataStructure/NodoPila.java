013a1abddf5ef9dfe7d04fd209d6b312e3ee5999
cesarandres17 <cesara.ramirezr@uqvirtual.edu.co>
2026-05-05 15:55:20 -0500

    feat: implementar estructura de datos pila para operaciones reversibles

---
+++ src/main/java/com/fintech/dbilleteras_virtuales/dataStructure/NodoPila.java
@@ -0,0 +1,27 @@
+package main.java.com.fintech.dbilleteras_virtuales.dataStructure;
+
+public class NodoPila<T> {
+    private T dato;
+    private NodoPila<T> siguiente;
+    
+    public NodoPila(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+    
+    public T getDato() {
+        return dato;
+    }
+    
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+    
+    public NodoPila<T> getSiguiente() {
+        return siguiente;
+    }
+    
+    public void setSiguiente(NodoPila<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+}
