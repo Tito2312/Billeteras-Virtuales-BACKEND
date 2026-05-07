1eeff6616c6c46a1f73641af14567c41dec0bb0e
cesarandres17 <cesara.ramirezr@uqvirtual.edu.co>
2026-05-07 10:56:10 -0500

    fix: corregir declaraciones de paquete en archivos dataStructure

--- src/main/java/com/fintech/dbilleteras_virtuales/dataStructure/ColaPrioridad.java
+++ src/main/java/com/fintech/dbilleteras_virtuales/dataStructure/ColaPrioridad.java
@@ -1,4 +1,4 @@
-package main.java.com.fintech.dbilleteras_virtuales.dataStructure;
+package com.fintech.dbilleteras_virtuales.dataStructure;
 
 public class ColaPrioridad<T extends Comparable<T>> {
     private NodoColaPrioridad<T> cabeza;
